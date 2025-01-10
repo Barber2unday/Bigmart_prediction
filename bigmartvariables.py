d239a0541ce616667a888c8e885b2ea388a1dd6e
Babz-bit <shobayopeterbabatunde@gmail.com>
2025-01-10 10:58:05 +0100

    new file for deployment

---
+++ bigmartvariables.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class bigmartsalespredict(BaseModel):
+    Item_MRP: float
+    Outlet_Type: float
+    Outlet_Identifier_OUT027: float
+    Outlet_Location_Type: float
+    Outlet_Identifier_OUT035: float
+    Outlet_age: float
+    Item_Visibility: float
+    Outlet_Size: float
+    Outlet_Identifier_OUT010: float
+    Outlet_Identifier_OUT019: float
