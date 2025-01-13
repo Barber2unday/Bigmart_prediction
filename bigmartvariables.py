859c81982b5ad2586560afba940d2ee4c5c0230d
Babz-bit <shobayopeterbabatunde@gmail.com>
2025-01-13 13:57:40 +0100

    code changes in bigmartvariables.py

--- bigmartvariables.py
+++ bigmartvariables.py
@@ -2,12 +2,12 @@ from pydantic import BaseModel
 
 class bigmartsalespredict(BaseModel):
     Item_MRP: float
-    Outlet_Type: float
+    Outlet_Type: int
     Outlet_Identifier_OUT027: float
-    Outlet_Location_Type: float
+    Outlet_Location_Type: int
     Outlet_Identifier_OUT035: float
-    Outlet_age: float
+    Outlet_age: int
     Item_Visibility: float
-    Outlet_Size: float
+    Outlet_Size: int
     Outlet_Identifier_OUT010: float
     Outlet_Identifier_OUT019: float
